4126daada20aafcb8476632158f4c30bebdd5260
littlecloud1 <cloudtang030@gmail.com>
2018-08-20 15:52:39 +0800

    add restaurant id variable in route

--- webserver-restaurant.py
+++ webserver-restaurant.py
@@ -4,17 +4,18 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 app = Flask(__name__)
 
-
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 DBsession = sessionmaker(bind=engine)
-session = DBsession()
 
 @app.route('/')
-@app.route('/hello')
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
 
-def HelloWorld():
-    restaurant = session.query(Restaurant).first()
+    #create a new thread for every cursor
+    session = DBsession()
+
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     output = ''
     for i in items:
